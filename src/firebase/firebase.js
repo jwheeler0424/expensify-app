fedafa009d56ad667bc78658aa03a4acd8783c89
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-07-11 15:40:47 -0700

    fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,29 +15,60 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Jonathan Wheeler',
-    age: 34,
-    stressLevel: 6,
-    job: {
-        title: 'Software Developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Fresno',
-        country: 'United States'
-    }
-}).then(() => {
-    console.log('Data is saved!')
-}).catch((e) => {
-    console.log('This failed.', e)
-});
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+})
 
 database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
+    name: 'Jonathan'
 });
+
+// setTimeout(() => {
+//     database.ref('age').set(33);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(35);
+// }, 10500);
+
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data.', e)
+//     });
+
+// database.ref().set({
+//     name: 'Jonathan Wheeler',
+//     age: 34,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Fresno',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('Data is saved!')
+// }).catch((e) => {
+//     console.log('This failed.', e)
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 // database.ref()
 //     .remove()
