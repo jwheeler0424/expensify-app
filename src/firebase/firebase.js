42ffd1c234982482a4d3d6ae8dfa536d5bd8c745
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-07-11 16:44:00 -0700

    array data in firebase: part 1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,66 +15,23 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on('value', (snapshot) => {
-    const val = snapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
-})
+const expenses = [{
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+}, {
+    description: 'Rent',
+    note: '',
+    amount: 109500,
+    createdAt: 123456789
+}, {
+    description: 'Credit Card',
+    note: '',
+    amount: 4500,
+    createdAt: 12345678912345
+}]
 
-database.ref().update({
-    name: 'Jonathan'
+expenses.forEach((expense) => {
+    database.ref('expenses').push(expense);
 });
-
-// setTimeout(() => {
-//     database.ref('age').set(33);
-// }, 3500);
-
-// setTimeout(() => {
-//     database.ref().off(onValueChange);
-// }, 7000);
-
-// setTimeout(() => {
-//     database.ref('age').set(35);
-// }, 10500);
-
-// database.ref('location/city')
-//     .once('value')
-//     .then((snapshot) => {
-//         const val = snapshot.val();
-//         console.log(val);
-//     })
-//     .catch((e) => {
-//         console.log('Error fetching data.', e)
-//     });
-
-// database.ref().set({
-//     name: 'Jonathan Wheeler',
-//     age: 34,
-//     stressLevel: 6,
-//     job: {
-//         title: 'Software Developer',
-//         company: 'Google'
-//     },
-//     location: {
-//         city: 'Fresno',
-//         country: 'United States'
-//     }
-// }).then(() => {
-//     console.log('Data is saved!')
-// }).catch((e) => {
-//     console.log('This failed.', e)
-// });
-
-// database.ref().update({
-//     stressLevel: 9,
-//     'job/company': 'Amazon',
-//     'location/city': 'Seattle'
-// });
-
-// database.ref()
-//     .remove()
-//     .then(() => {
-//         console.log('Data was removed')
-//     })
-//     .catch((e) => {
-//         console.log('Did not remove data', e)
-//     });
