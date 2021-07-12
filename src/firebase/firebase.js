bbb4d0c62c6e03242fca16e718b3616618449cc9
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-07-12 11:03:11 -0700

    creating a separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAYsT8-AO2qYT8c4E6wlPfupo155KMsevU",
-    authDomain: "expensify-4d7e6.firebaseapp.com",
-    databaseURL: "https://expensify-4d7e6-default-rtdb.firebaseio.com",
-    projectId: "expensify-4d7e6",
-    storageBucket: "expensify-4d7e6.appspot.com",
-    messagingSenderId: "457348987732",
-    appId: "1:457348987732:web:39cd1f931b121dc65fe6c1",
-    measurementId: "G-C91N9FY26C"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
